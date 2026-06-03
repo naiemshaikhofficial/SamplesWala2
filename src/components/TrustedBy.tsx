11b11a20f07b24e13aa71b6da08bf6d59958e3f0
Naiem Shaikh <naiemshaikh@gmail.com>
2026-06-03 22:00:15 +0530

    feat: add TrustedBy marquee component to display partner music labels

--- src/components/TrustedBy.tsx
+++ src/components/TrustedBy.tsx
@@ -5,7 +5,7 @@ const labels = [
   { name: 'Sony Music', logo: '/logos/sony.png', scale: 'scale-[1.1]' },
   { name: 'T-Series', logo: '/logos/tseries.png', scale: 'scale-[0.85]' },
   { name: 'Zee Music', logo: '/logos/zee.png', scale: 'scale-[1.35]' },
-  { name: 'Desi Music Factory', logo: '/logos/desi.png', scale: 'scale-[1.0]' },
+  { name: 'Desi Music Factory', logo: '/logos/desi.png', scale: 'scale-[1.1]' },
   { name: 'Tips', logo: '/logos/tips.png', scale: 'scale-[1.1]' },
   { name: 'Saregama', logo: '/logos/saregama.png', scale: 'scale-[1.2]' },
   { name: 'White Hill', logo: '/logos/hill.png', scale: 'scale-[1.0]' },
@@ -16,7 +16,7 @@ const labels = [
   { name: 'Warner Music India', logo: '/logos/warner.png', scale: 'scale-[1.2]' },
   { name: 'Ultra Media', logo: '/logos/ultra.png', scale: 'scale-[1.1]' },
   { name: 'Times Music', logo: '/logos/times.png', scale: 'scale-[1.2]' },
-  { name: 'Universal Music Group', logo: '/logos/universal.png', scale: 'scale-[1.7] invert' },
+  { name: 'Universal Music Group', logo: '/logos/universal.png', scale: 'scale-[1.8] invert' },
 ];
 
 export default function TrustedBy() {
